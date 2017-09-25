8b170eddc86d205dbfc17dc71a39d910c369dc6b
Tom Söderlund <tom@tomsoderlund.com>
2017-09-25 23:18:13 +0200

    UI to add kittens

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,36 @@ export default class IndexPage extends React.Component {
 			const url = `${req.headers.referer}api/kittens`;
 			const response = await fetch(url);
 			const kittensJson = await response.json();
-			return { kittens: kittensJson };
+			return { apiUrl: url, kittens: kittensJson };
 		}
 		catch (err) {
 			return { error: 'Could not load kittens' }
 		}
 	};
+
+	constructor(props) {
+		super(props)
+		this.state = { name: '' }
+	}
+
+	handleChange (event) {
+		this.setState({ name: event.target.value });
+	}
+
+	handleAdd (event) {
+		fetch(this.props.apiUrl, {
+			method: 'POST',
+			body: JSON.stringify({
+				name: this.state.name,
+			}),
+			headers: {
+				'Accept': 'application/json',
+				'Content-Type': 'application/json',
+			},
+		})
+		.then(results => console.log('Fetch results', results))
+		.catch(err => console.error('Fetch error', err));
+	}
 
 	render () {
 
@@ -21,6 +45,8 @@ export default class IndexPage extends React.Component {
 		return <div>
 			<h1>Kittens</h1>
 			{kittenList}
+			<input value={this.state.name} onChange={this.handleChange.bind(this)} placeholder='Enter a kitten name'/>
+			<button onClick={this.handleAdd.bind(this)}>Add kitten</button>
 		</div>
 	};
 
