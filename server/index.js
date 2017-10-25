7c198d83ba1196c0d7226c0117fe1c20181ff10d
Tom Söderlund <tom@tomsoderlund.com>
2017-10-25 19:54:34 +0200

    Single quotes

--- server/index.js
+++ server/index.js
@@ -21,8 +21,8 @@ app.prepare().then(() => {
 
 	// Allows for cross origin domain request:
 	server.use(function(req, res, next) {
-		res.header("Access-Control-Allow-Origin", "*");
-		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		res.header('Access-Control-Allow-Origin', '*');
+		res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 		next();
 	});
 
